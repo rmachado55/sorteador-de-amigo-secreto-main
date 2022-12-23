230c208323da2faa69481f1c7ffd1e6008192289
Ricardo <ricardo.machado7@hotmail.com>
2022-12-23 11:18:52 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import Formulario from './componentes/Formulario';
+import Configuracao from './paginas/Configuracao';
+import Sorteio from './paginas/Sorteio';
 
 function App() {
   return (
-   <BrowserRouter>
-    <RecoilRoot>
-      <Routes>
-        <Route path='/' element={<Formulario/>} />
-      </Routes>
-    </RecoilRoot>
-   </BrowserRouter>
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path='/' element={<Configuracao />}/>
+          <Route path='/sorteio' element={<Sorteio />}/>
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 }
 
